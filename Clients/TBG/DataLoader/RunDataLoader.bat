ba21bce2e45180fa6d3c81b7e6a1e893e49b9ad5
Dave Boyce <daveb@501commons.org>
2019-03-19 17:43:13 -0700

    tbg update

--- Clients/TBG/DataLoader/RunDataLoader.bat
+++ Clients/TBG/DataLoader/RunDataLoader.bat
@@ -1,3 +1,3 @@
 echo incoming %1 %2 %3
-cd "C:\repo\Salesforce-Exporter-Private\Clients\%2\Salesforce-Exporter\Clients\%2\dependencies\salesforce.com\dataloader\v44\bin"
+cd "C:\repo\Salesforce-Exporter-Private\Clients\%2\Salesforce-Exporter\dependencies\salesforce.com\dataloader\v44\bin"
 call process C:\repo\Salesforce-Exporter-Private\Clients\%2\Salesforce-Exporter\Clients\%2\DataLoader\ %3_%1
